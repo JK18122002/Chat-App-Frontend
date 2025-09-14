e81f0cd25a07e4e020f8bcaf36e23cf46280a481
Jitendra Kumar <jitendrasihalinagar5@gmail.com>
2025-09-14 19:45:09 +0530

    Url

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 import toast from 'react-hot-toast'
 import { io } from 'socket.io-client'
 
-const backendUrl = import.meta.env.VITE_BACKEND_URL
+const backendUrl = import.meta.env.URL
 axios.defaults.baseURL = backendUrl
 
 export const AuthContext = createContext()
